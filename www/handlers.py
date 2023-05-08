fd37b3056ac723f12d3226fc66fcf79b8f66b9c4
alanfanh <alan.fan.h@qq.com>
2023-05-08 18:50:29 +0800

    新增用户注册、日志列表、日志详情等API函数，完善user2cookie函数

--- www/handlers.py
+++ www/handlers.py
@@ -4,13 +4,14 @@
 import hashlib
 import json
 import logging
+import re
 import time
 
 import markdown
 from aiohttp import web
 from coroweb import get, post
-from models import User, Blog, Comment
-from apis import Page, APIValueError, APIResourceNotFoundError, APIPermissionError
+from models import User, Blog, Comment, next_id
+from apis import Page, APIValueError, APIResourceNotFoundError, APIPermissionError, APIError
 from config import configs
 
 COOKIE_NAME = 'awesession'
@@ -39,6 +40,8 @@ def user2cookie(user, max_age):
     # 使用id-expires-sha1构建cookie字符串
     expires = str(int(time.time() + max_age))
     s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
+    value_l = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
+    return '-'.join(value_l)
 
 
 @get('/')
@@ -229,3 +232,53 @@ def api_get_user(*, page="1"):
     for u in users:
         u.passwd = '******'
     return dict(page=p, users=users)
+
+
+# 定义EMAIL和HASH格式规范
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
+
+
+# 用户注册API
+@post('api/users')
+async def api_register_user(*, email, name, passwd):
+    if not name or not name.strip():
+        raise APIValueError('name')
+    if not email or _RE_EMAIL.match(email):
+        raise APIValueError('email')
+    if not passwd or _RE_SHA1.match(passwd):
+        raise APIValueError('passwd')
+    users = await User.findAll('email=?', [email])
+    if len(users) > 0:
+        raise APIError('register:failed', 'email', 'Email is already in use')
+    uid = next_id()
+    sha1_passwd = '%s:%s' % (uid, passwd)
+    user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),
+                image='http://www.gravatar.com/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
+    await user.save()
+    # make session cookie
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.passwd = '*******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
+
+
+# 获取日志列表API
+@get('/api/blogs')
+async def api_blogs(*, page='1'):
+    page_index = get_page_index(page)
+    num = await Blog.findNumber('count(id)')
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p, blogs=())
+    blogs = await Blog.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+    return dict(page=p, blogs=blogs)
+
+
+# 获取日志详情API
+@get('/api/blogs/{id}')
+async def api_get_blog(*, pk):
+    blog = await Blog.find(pk)
+    return blog
